8487acf5fcd25a34c371da215d41dda0d92810a9
Fantasma <cmunoz@thebrandindustry.com>
2022-02-25 08:14:47 -0600

    animacion

---
+++ src/js/homeAnimation.js
@@ -0,0 +1,52 @@
+ var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop:true,
+    grabCursor: false,
+    draggable: false,
+    noSwiping: true,
+    autoplay: {
+        delay: 2500,
+    },
+});
+
+const animationSlideUp = [
+    {
+        transform: 'translateY(100px)',
+        opacity: 0,
+    },
+    {
+        transform: 'translateY(0px)',
+        opacity: 1,
+
+    }
+]
+
+const animationStroke= [
+    {
+        width: '1%',
+        position: 'relative',
+        top: '-30px',
+        opacity: 0,
+    },
+    {
+        width: '100%',
+        position: 'relative',
+        top: '-30px',
+        opacity: 1,
+
+    }
+]
+
+swiper.on('slideChange', function (e) {
+    const index = swiper.activeIndex + 1
+    const title = document.querySelector(`.swiper-slide:nth-child(${index}) >.heading >div > p`)
+    const subTitle = document.querySelector(`.swiper-slide:nth-child(${index}) >.heading > div:first-of-type + div >  p`)
+    const img = document.querySelector(`.swiper-slide:nth-child(${index}) >.heading > div:last-child >  img`)
+
+
+    title.animate(animationSlideUp, { duration: 900, fill: 'both',easing:'cubic-bezier(0.65, 0, 0.35, 1)' })
+    subTitle.animate(animationSlideUp, { duration: 900, fill: 'both' ,easing:'cubic-bezier(0.65, 0, 0.35, 1)' })
+    img.animate(animationStroke, { duration: 900, fill: 'both' ,easing:'cubic-bezier(0.65, 0, 0.35, 1)' })
+
+   
+});
