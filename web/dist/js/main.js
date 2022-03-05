785b52e6fbc316f17f575cdd4906618cda152d41
Juan Jose <juanjosezg@gmail.com>
2022-03-05 07:00:59 -0800

    Social

--- web/dist/js/main.js
+++ web/dist/js/main.js
@@ -491,47 +491,5 @@ d3.select(this).attr("fill",determineFill).call(function(d){var data=d.datum().d
 'use strict';var h=document.getElementById("rowBottom");var stuck=false;var stickPoint=getDistance();function getDistance(){var topDist=h.offsetTop;return topDist;}window.onscroll=function(e){var distance=getDistance()-window.pageYOffset;var offset=window.pageYOffset;if(distance<=0&&!stuck){h.style.position='fixed';h.style.top='0px';stuck=true;h.classList.add("show-logo");h.classList.remove("hide-logo");}else if(stuck&&offset<=stickPoint){h.style.position='static';//static
 stuck=false;h.classList.add("hide-logo");h.classList.remove("show-logo");}};// Hamburger menu
 var hamburgerButton=document.querySelector(".hamburger");var hamburgerMenu=document.querySelector(".mobile-nav");var hamburgerMenuText=document.querySelector(".menuText");hamburgerButton.onclick=function(){this.classList.toggle("is-active");hamburgerMenu.classList.toggle("show-menu");hamburgerMenuText.classList.toggle("hideTextMenu");};
-"use strict";var $body;window.addEventListener("load",function(){videoOnLoad();});function videoOnLoad(){var ytSrc="https://www.youtube.com/iframe_api",vimSrc="https://player.vimeo.com/api/player.js";$body=$('body');loadScript(ytSrc);loadScript(vimSrc);}function loadScript(src,callback){callback=typeof callback==="function"?callback:function(){return null;};var tag=document.createElement('script');tag.src=src;tag.onload=function(){callback();};var firstScriptTag=document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);}if(document.querySelector('.videoBlock-video .video-player-v2.vimeo')){document.querySelectorAll('.videoBlock-video .video-player-v2.vimeo:not(.video-initialized)').forEach(function(item){item.addEventListener('click',function(e){var target=e.target;var videoId=target.parentNode.parentNode.dataset.videoid;target.parentNode.parentNode.classList.add('videoStart');var iframeDiv=document.querySelector('#iframe__'+videoId);var options={id:videoId,width:840,responsive:true};var player=new Vimeo.Player(iframeDiv,options);player.setVolume(0.5);player.play();});});}if(document.querySelector('.videoBlock-video .video-player-v2.youtube')){document.querySelectorAll('.videoBlock-video .video-player-v2.youtube:not(.video-initialized)').forEach(function(item){item.addEventListener('click',function(e){var targetYT=e.target;var videoId=targetYT.parentNode.parentNode.dataset.videoid;targetYT.parentNode.parentNode.classList.add('videoStart');var iframeDiv=document.querySelector('#iframe__'+videoId);var player;player=new YT.Player(iframeDiv,{videoId:videoId,playerVars:{autoplay:0,controls:1,info:0,showinfo:0,rel:0,modestbranding:1,wmode:"transparent"},events:{'onReady':onPlayerReady,'onStateChange':onPlayerStateChange}});function onPlayerReady(event){event.target.playVideo();}var done=false;function onPlayerStateChange(event){if(event.data==YT.PlayerState.PLAYING&&!done){setTimeout(stopVideo,6000);done=true;}}function stopVideo(){player.stopVideo();}});});/*
-  document.querySelector('.videoBlock-video .video-player-v2.youtube:not(.video-initialized)').addEventListener('click', function (e) { 
-  
-    var targetYT = e.target;
-    var videoId = targetYT.parentNode.parentNode.dataset.videoid;
-    targetYT.parentNode.parentNode.classList.add('videoStart');
-    var iframeDiv = document.querySelector('#iframe__' + videoId);
-    var player;
-    player = new YT.Player(iframeDiv, {
-      videoId: videoId,
-      playerVars: {
-        autoplay: 0,
-        controls: 1,
-        info: 0,
-        showinfo: 0,
-        rel: 0,
-        modestbranding: 1,
-        wmode: "transparent"
-      },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
-
-  var done = false;
-
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
-    }
-  }
-
-  function stopVideo() {
-    player.stopVideo();
-  }
-
-});*/}
+"use strict";var $body;window.addEventListener("load",function(){videoOnLoad();});function videoOnLoad(){var ytSrc="https://www.youtube.com/iframe_api",vimSrc="https://player.vimeo.com/api/player.js";$body=$('body');loadScript(ytSrc);loadScript(vimSrc);}function loadScript(src,callback){callback=typeof callback==="function"?callback:function(){return null;};var tag=document.createElement('script');tag.src=src;tag.onload=function(){callback();};var firstScriptTag=document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);}if(document.querySelector('.videoBlock-video .video-player-v2.vimeo')){document.querySelectorAll('.videoBlock-video .video-player-v2.vimeo:not(.video-initialized)').forEach(function(item){item.addEventListener('click',function(e){var target=e.target;var videoId=target.parentNode.parentNode.dataset.videoid;target.parentNode.parentNode.classList.add('videoStart');var iframeDiv=document.querySelector('#iframe__'+videoId);var options={id:videoId,width:840,responsive:true};var player=new Vimeo.Player(iframeDiv,options);player.setVolume(0.5);player.play();});});}if(document.querySelector('.videoBlock-video .video-player-v2.youtube')){document.querySelectorAll('.videoBlock-video .video-player-v2.youtube:not(.video-initialized)').forEach(function(item){item.addEventListener('click',function(e){var targetYT=e.target;var videoId=targetYT.parentNode.parentNode.dataset.videoid;targetYT.parentNode.parentNode.classList.add('videoStart');var iframeDiv=document.querySelector('#iframe__'+videoId);var player;player=new YT.Player(iframeDiv,{videoId:videoId,playerVars:{autoplay:0,controls:1,info:0,showinfo:0,rel:0,modestbranding:1,wmode:"transparent"},events:{'onReady':onPlayerReady,'onStateChange':onPlayerStateChange}});function onPlayerReady(event){event.target.playVideo();}var done=false;function onPlayerStateChange(event){if(event.data==YT.PlayerState.PLAYING&&!done){setTimeout(stopVideo,6000);done=true;}}function stopVideo(){player.stopVideo();}});});}
 "use strict";$(function(){$(".VideoCta").each(initVideoCta);function initVideoCta(elem){var $this=$(this);var modal=$this.find(".VideoCta-modal");$this.find("button").on("click",open);$this.find(".VideoCta-close").on("click",close);function open(){modal.addClass("modal--open");}function close(){modal.removeClass("modal--open");}}});
