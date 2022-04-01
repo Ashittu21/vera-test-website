a6f695eac6ac46cb29fbfd5b7f76e86025079d1f
Juan Jose <juanjosezg@gmail.com>
2022-04-01 13:39:53 -0700

    Search

--- src/js/searchToggle.js
+++ src/js/searchToggle.js
@@ -1,11 +1,22 @@
-function searchToggle(obj, evt){
-  var container = $(obj).closest('.search-wrapper');
+/*function searchToggle(obj, evt){
+  var container = $('.search-wrapper');
       if(!container.hasClass('active')){
           container.addClass('active');
           evt.preventDefault();
-      }
-      else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+      } else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
           container.removeClass('active');
           container.find('.search-input').val('');
       }
+}*/
+
+
+function openSearch(e) { 
+    var container = $('#search-wrapper');
+    if(!container.hasClass('active')){
+        container.addClass('active');
+        e.preventDefault();
+    } else if(container.hasClass('active') && container.closest('.input-holder').length == 0){
+        container.removeClass('active');
+        container.find('.search-input').val('');
+    }
 }
