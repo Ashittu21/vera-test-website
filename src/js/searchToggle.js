65150ec380d138309a00369ec341cab6501f0d8c
Juan Jose <juanjosezg@gmail.com>
2022-04-01 15:02:46 -0700

    Search

--- src/js/searchToggle.js
+++ src/js/searchToggle.js
@@ -12,11 +12,13 @@
 
 function openSearch(e) { 
     var container = $('#search-wrapper');
+
     if(!container.hasClass('active')){
         container.addClass('active');
         e.preventDefault();
-    } else if(container.hasClass('active') && container.closest('.input-holder').length == 0){
+    } else if (container.hasClass('active') && container.closest('.input-holder').length == 0) {
         container.removeClass('active');
         container.find('.search-input').val('');
+        e.preventDefault();
     }
 }
