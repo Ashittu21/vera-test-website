8657f1bb791346c7ffa64bf8b3b2799b6ef1111c
Juan José <juanjosezg@gmail.com>
2022-04-07 07:36:56 -0700

    Bugherd

--- src/js/detect-iOS.js
+++ src/js/detect-iOS.js
@@ -3,9 +3,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   var contentTag = document.getElementsByTagName('main');
+
+  function isRetinaDisplay() {
+    if (window.matchMedia) {
+        var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+        return (mq && mq.matches || (window.devicePixelRatio > 1)); 
+    }
+  }
+
   var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
   if (isIOS) {
     contentTag[0].classList.add('iOS');
   } 
+
+  if (isRetinaDisplay) { 
+    contentTag[0].classList.add('retina');
+  }
 
 });
