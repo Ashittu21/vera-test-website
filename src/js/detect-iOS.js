30492cdef2ee9360a9938e4b1669280722ce8d1f
Juan José <juanjosezg@gmail.com>
2022-04-07 09:11:20 -0700

    Bugherd

--- src/js/detect-iOS.js
+++ src/js/detect-iOS.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var contentTag = document.getElementsByTagName('main');
   var isMacLike = /(Mac|iPad)/i.test(navigator.platform);
   var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  
+  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
   function isRetinaDisplay() {
     if (window.matchMedia) {
         var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     contentTag[0].classList.add('iOS');
   } 
 
-  if (isRetinaDisplay && isMacLike ) { 
+  if (isRetinaDisplay && isMacLike && isSafari) { 
     contentTag[0].classList.add('retina');
   }
 
