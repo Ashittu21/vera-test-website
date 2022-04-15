54e34620893c85ae297df98493aac6cced77c664
Juan José <juanjosezg@gmail.com>
2022-04-15 09:17:48 -0700

    Hero

--- src/js/v2-homeSlider.js
+++ src/js/v2-homeSlider.js
@@ -2,17 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (document.querySelector('.heroType3')) {
     
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".homeSlider", {
       direction: "vertical",
       loop: true,
-      grabCursor: false,
-      draggable: false,
+      grabCursor: false, //false
+      draggable: false, //false
       noSwiping: true,
       preventInteractionOnTransition: false,
-      allowTouchMove: false,
+      allowTouchMove: false, //false
       slideToClickedSlide: false,
-      preventClicksPropagation: false,
-      preventClicks: false,
+      preventClicksPropagation: false,  //false
+      preventClicks: false, //false
+     // autoplay: false,
       autoplay: {
         delay: 2700,
       },
@@ -21,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const wordWidth = "100%";
 
     const animationStroke = [{
-      width: '1%',
+      width: '0px',
       position: 'relative',
       top: '-30px',
       opacity: 0,
@@ -46,28 +47,34 @@ document.addEventListener('DOMContentLoaded', function () {
     ]
 
     swiper.on('slideChange', function (e) {
-      const index = swiper.activeIndex + 1
-      const title = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero >div > p`)
-      const subTitle = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div:first-of-type + div >  p`)
-      const img = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div:last-child >  img`)
-      const lastWord = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div:nth-of-type(2) p.slide-up`)
-      const wordWidth = lastWord.clientWidth + "px"
-      const animationStroke = [{width: '1%',},{width: wordWidth,}]
-            title.animate(animationSlideUp, {
-              duration: 450,
-              fill: 'both',
-              easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
-            })
-            subTitle.animate(animationSlideUp, {
-              duration: 900,
-              fill: 'both',
-              easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
-            })
-            img.animate(animationStroke, {
-              duration: 1800,
-              fill: 'both',
-              easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
-            })
+
+      const index = swiper.activeIndex + 1;
+      const title = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div.textWrap > div.text`);
+      const subTitle = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div:first-of-type + div >div`);
+      const underline = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero > div:nth-of-type(2) span.heroUnderline`);
+      const img = document.querySelector(`.swiper-slide:nth-child(${index}) >.headingHero >  div:nth-of-type(2) span.heroUnderline  img`);
+
+      const wordWidth = underline.clientWidth + "px";
+
+      const animationStroke = [{ width: '0px', }, { width: wordWidth, }];
+
+      title.animate(animationSlideUp, {
+        duration: 450,
+        fill: 'both',
+        easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
+      });
+      
+       subTitle.animate(animationSlideUp, {
+        duration: 900,
+        fill: 'both',
+        easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
+      });
+            
+      img.animate(animationStroke, {
+        duration: 1800,
+        fill: 'both',
+        easing: 'cubic-bezier(0.65, 0, 0.35, 1)'
+      });
 
 
     });
