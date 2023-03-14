b1bc1617c25119f2b58684cfb60985326a48389e
juanjosezg <juanjosezg@gmail.com>
2023-03-14 11:07:07 -0700

    Job Form

--- web/get-job.php
+++ web/get-job.php
@@ -1,11 +1,15 @@
 <?php
  require '../vendor/autoload.php';
  $client = new GuzzleHttp\Client();
+
  $jobid= filter_var($_GET['id'], FILTER_VALIDATE_INT);
- $res = $client->request('GET', 'https://boards-api.greenhouse.io/v1/boards/verainstituteofjustice/jobs/'.$jobid, [
+ $res = $client->request('GET',
+ 'https://boards-api.greenhouse.io/v1/boards/verainstituteofjustice/jobs/'.$jobid,
+ [
     'auth' => ['0c0c13ae647efe172323492bce235dd3-5', ''],
-    'query' => ['content' => 'true']
-]);
+    'query' => ['content' => 'true', 'questions'=>'true']
+ ]);
+
 echo $res->getBody();
 ?>
 
---
+++ web/post-job.php
@@ -0,0 +1,28 @@
+<?php
+require '../vendor/autoload.php';
+$client = new GuzzleHttp\Client();
+$jobid = filter_var($_POST['id'], FILTER_VALIDATE_INT);
+header('Content-Type: application/json');
+$form_data = [];
+
+foreach ($_POST as $key => $value) {
+    $form_data[$key] = $value;
+}
+
+try {
+    $response = $client->request('POST', 'https://boards-api.greenhouse.io/v1/boards/verainstituteofjustice/jobs/'.$jobid, [
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+        'auth' => ['969bfbf42a90587a1e7ad06d80003db4-5', ''],
+        'json' => $form_data
+    ]);
+    $body = $response->getBody();
+    echo $body;
+} catch (GuzzleHttp\Exception\ClientException $e) {
+    // handle the exception
+    $response = $e->getResponse();
+    $responseBodyAsString = $response->getBody()->getContents();
+    echo $responseBodyAsString;
+}
+?>
